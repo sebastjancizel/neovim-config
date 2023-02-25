4b513132b8ec4a823f9a0bce1cde5b15f2e3f6f5
Sebastjan Cizel <sebastjancizel@gmail.com>
2023-02-25 14:03:35 +0000

    Remove code in example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,14 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {
+if true then return {} end
+
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -24,16 +32,6 @@ if true then return {
       },
     },
   },
-
-} end
-
--- every spec file under config.plugins will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-return {
 
   -- change trouble config
   {
